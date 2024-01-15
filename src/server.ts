20695a6bf29ab9b46160d774fc393b81b86066b6
Ahmad Waldi Arridho <ahmad.waldi@digits.id>
2024-01-16 01:29:18 +0700

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { App } from './app';
+import { ValidateEnv } from '@utils/validateEnv';
+import { UserRoute } from '@routes/users.route';
+import { AuthRoute } from '@routes/auth.route';
+
+ValidateEnv();
+
+const app = new App([new UserRoute(), new AuthRoute()]);
+
+app.listen();
